36004a07d1ede1f48b647905db41fa8f8884202f
Big Brother <aykibugz@yandex.ru>
2025-03-18 20:38:03 +0400

    Decomposition

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,1 +1,10 @@
-﻿
+﻿import subprocess
+
+
+def main():
+    path = "src/gen/execute/gen_cpp.exe"
+    output = subprocess.run(path, capture_output=True, text=True)
+    print(output.stdout)
+
+if __name__ == "__main__":
+    main()
