eb5d6b55573c7f0487851eb0cfcf0ad20a546605
Big Brother <aykibugz@yandex.ru>
2025-04-12 17:49:29 +0400

    add argparse

--- lab_3/main.py
+++ lab_3/main.py
@@ -4,6 +4,18 @@ from src.atomic.rsa import RSA
 from src.atomic.cast5 import CAST5
 
 
+def valid_args(args: argparse.Namespace):
+    count = 0
+    methods = ("generate", "encrypt", "decrypt")
+
+    for arg in methods:
+        if getattr(args, arg) is not None:
+            count += 1
+
+    if count > 1:
+            raise ValueError(f"You can use only on of them: {methods}")
+
+
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="",
@@ -14,11 +26,16 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("--encrypt", "-e", help="")
     parser.add_argument("--decrypt", "-d", help="")
 
-    return parser.parse_args()
+    args = parser.parse_args()
+    valid_args(args)
+
+    return args
 
 
 def main():
+    args = get_args()
 
+    print(args)
 
 if __name__ == "__main__":
     main()
