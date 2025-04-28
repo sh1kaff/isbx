da0cac83484110158b5ab00b7f48f855a5f3d028
Big Brother <aykibugz@yandex.ru>
2025-04-28 21:34:19 +0400

    Add get_crack_time

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,5 +1,6 @@
 ﻿import logging
 from src.hash_cracker import crack_hash_with_mp
+from src.crack_time import  get_crack_time
 
 from config.config import USER_INFO
 
@@ -9,16 +10,17 @@ def main():
         level=logging.INFO
     )
 
-    user_info_bin = USER_INFO["bin"]
-    for bin in user_info_bin:
-        result = crack_hash_with_mp(
-            USER_INFO["hash"],
-            USER_INFO["last_digits"],
-            bin
-        )
+    # user_info_bin = USER_INFO["bin"]
+    # for bin in user_info_bin:
+    #     result = crack_hash_with_mp(
+    #         USER_INFO["hash"],
+    #         USER_INFO["last_digits"],
+    #         bin
+    #     )
 
-        if result:
-            break
+    #     if result:
+    #         break
+    get_crack_time("140495200b351b7f18a46e3796f2875ebdf0023568933ef3b99efb285af3f06b","2301", "547905")
 
 if __name__ == "__main__":
     main()
---
+++ lab_4/src/crack_time.py
@@ -0,0 +1,22 @@
+﻿import time
+import psutil
+import multiprocessing as mp
+
+from src.hash_cracker import crack_hash_with_mp
+
+
+def get_crack_time(
+    hash: str,
+    last_digits: str,
+    bin: str
+) -> int:
+    real_cores = psutil.cpu_count(logical=False)
+
+    for cores in range(1, int(real_cores * 1.5) + 1):
+        start_time = time.time()
+        crack_hash_with_mp(hash, last_digits, bin, cores)
+        print(f"Time: {time.time() - start_time}")
+
+
+if __name__ == "__main__":
+    get_crack_time("140495200b351b7f18a46e3796f2875ebdf0023568933ef3b99efb285af3f06b","2301", "547905")
